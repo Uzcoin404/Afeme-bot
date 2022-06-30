6b41ac4e85b6e3191c6bab8ebaab1c8a5f3ffca4
Uzcoin404 <uzcointg@gmail.com>
2022-06-30 10:10:11 +0500

    some fixes

--- index.php
+++ index.php
@@ -15,6 +15,8 @@ $telegram = new Telegram("5297964553:AAF8H_f2UB3xXI0TVWRJMKjdrGXYKoJY0mk", true)
 $chatID = "-1001760618618";
 $func = new Functions();
 $Admin = "829349149";
+$postID = $_GET['post_id'];
+
 // $telegram->setWebhook($siteUrl);
 
 $message = isset($telegram->getData()['message']) ? $telegram->getData()['message'] : '';
@@ -27,30 +29,27 @@ $fullName = $firstName . ' ' . $lastName;
 $data = json_decode(file_get_contents("http://ali98.uz/api/post"));
 
 if (is_array($data) && !empty($data)) {
-
     
-    if (is_array($data) && !    empty($data)) {
+    $images = $data[0]->image;
+    $advertID = $data[0]->id;
+    $saleName = $data[0]->sale_id->name_uz;
+    $priceSum = $data[0]->price_som;
+    $priceUsd = $data[0]->price_usd;
+    $room = $data[0]->room;
+    $htypeName = $data[0]->htype_id->name_uz;
+    $region = $data[0]->region_id->name_uz;
+    $city = $data[0]->city_id->name_uz;
+    $street = $data[0]->street;
 
-        $images = $data[0]->image;
-        $postID = $data[0]->id;
-        $saleName = $data[0]->sale_id->name_uz;
-        $priceSum = $data[0]->price_som;
-        $priceUsd = $data[0]->price_usd;
-        $room = $data[0]->room;
-        $htypeName = $data[0]->htype_id->name_uz;
-        $region = $data[0]->region_id->name_uz;
-        $city = $data[0]->city_id->name_uz;
-        $street = $data[0]->street;
-
-        if (!empty($images)) {
-            // for ($i = 0; $i < count($images); $i++) {
-                
-            // }
-            $func->toChannelPhoto($images[0]->url, $saleName . "ga $room xonali $htypeName sotiladi
+    if (!empty($images)) {
+        // for ($i = 0; $i < count($images); $i++) {
+            
+        // }
+        $func->toChannelPhoto($images[0]->url, $saleName . "ga $room xonali $htypeName sotiladi
 $priceSum so'm  $priceUsd$
 $region viloyati $city $street ko'chasi
-[Batafsil ko'rish](http://ali98.uz/api/post/$postID)");
-        }
+[Batafsil ko'rish](http://ali98.uz/api/post/$advertID)");
     }
+    print("<pre>" . print_r($data[0]->sale_id->name_uz, true) . "</pre>");
 }
-print("<pre>" . print_r($data[0]->sale_id->name_uz, true) . "</pre>");
+?>
--- user.php
+++ user.php
@@ -2,26 +2,6 @@
     class UserData {
         public $chatID;
 
-        public function __construct($chatID){
-            $this->chatID = $chatID;
-        }
-
-        public function setLang($data){
-            file_put_contents("users/lang/$this->chatID.txt", $data);
-        }
-        
-        public function getLang(){
-            return file_get_contents("users/lang/$this->chatID.txt");
-        }
-
-        public function setPage($data){
-            file_put_contents("users/page/$this->chatID.txt", $data);
-        }
-
-        public function getPage(){
-            return file_get_contents("users/page/$this->chatID.txt");
-        }
-
         public function setData($path, $data){
             file_put_contents("users/$path/$this->chatID.txt", $data);
         }
@@ -30,5 +10,4 @@
             return file_get_contents("users/$path/$this->chatID.txt");
         }
     }
-    
 ?>
